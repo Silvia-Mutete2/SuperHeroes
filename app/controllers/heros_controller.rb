8d3a44c2f723e07ea4dcb1af80d7f5e09d96f434
Silvia-Mutete <silvia.mutete@student.moringaschool.com>
2022-10-08 13:10:34 +0300

    POST request

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,9 +1,14 @@
 class HerosController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
-    
+
     def index 
         heros = Hero.all 
         render json: heros, status: :created
+    end
+
+    def show
+        hero = Hero.find(params[:id])
+        render json: hero,  status: :created
     end
 
 
